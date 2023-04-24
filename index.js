c5e5dff065591257fc93067be13478f519bf2d9a
jhlee409 <jhlee409@gmail.com>
2023-04-24 11:12:22 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>제목</title>
+	<meta charset="UTF-8">
+	<style>
+		h1 {
+			color: blue;
+		}
+	</style>
+</head>
+<body>
+	<h1>안녕하세요</h1>
+	<p>이것은 간단한 HTML 페이지입니다.</p>
+	<img src="image.jpg" alt="이미지">
+	<a href="https://www.example.com">링크</a>
+</body>
+</html>
